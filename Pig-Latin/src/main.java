ab06c80a16825580eb77a65b776b8caaf9904c6b
Lam, Alice <Alice_Lam@Dell.com>
2019-01-04 15:54:43 -0600

    pig latin project

---
+++ Pig-Latin/src/main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello. What word would you like to translate to pig latin?");
+		Scanner reader = new Scanner(System.in);
+		String input = reader.nextLine();
+		String result = parseWord(input);
+		System.out.println(result);
+		reader.close();
+	}
+	public static String parseWord(String input) {
+		char[] vowels = {'a','e','i','o','u'};
+		char firstLetter = input.charAt(0);
+		
+		//First letter is a vowel
+		for (int i= 0; i<vowels.length; i++) {
+			if (firstLetter == vowels[i]) {
+				return input + "yay";
+			}
+		}
+		
+		//First letter is not a vowel, but contains a vowel
+		for(int j=0; j<input.length(); j++) {
+			for (int i= 0; i<vowels.length; i++) {
+				if (input.charAt(j) == vowels[i]) {
+					String part1= input.substring(j, input.length());
+					String part2= input.substring(0, j);
+					String converted = part1 + part2 + "ay";
+					return converted;
+				}
+			}
+		}
+		
+		//if no vowels in the word at all
+		return input + "ay";
+	}
+}
