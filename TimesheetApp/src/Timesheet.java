c6e76d6407fb50f9eaa99ceb38bb6ec96f5e3b08
Lam, Alice <Alice_Lam@Dell.com>
2019-02-08 14:48:52 -0600

    initial timesheet application

---
+++ TimesheetApp/src/Timesheet.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timesheet {
+	private List<TimesheetEntry> database;
+	
+	public Timesheet() {
+		database = new ArrayList();
+	}
+}
